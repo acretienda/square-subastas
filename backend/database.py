6c5d5b1210b69f97180dc2429f7059c6567e6ce2
acretienda <acre.tienda@gmail.com>
2025-09-19 00:59:34 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # ⚡ Coge la URL de la base de datos desde la variable de entorno
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://user:password@localhost:5432/mydb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://subastas_db_user:fJc1OootrjKeyIHxaIsj6ps0DUnjhY9r@dpg-d33j79idbo4c73b97s1g-a:5432/subastas_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
