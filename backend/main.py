dfcb266c61c2be0033b3392813c9a83d2d17cb86
acretienda <acre.tienda@gmail.com>
2025-09-19 00:31:39 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,101 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from .database import Base, engine, get_db
-from . import models, crud
+A.1. INFORMACIÓN GENERAL
 
-Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Square Auctions API")
 
-@app.get('/')
-def read_root():
-    return {'message': 'API de subastas funcionando'}
+ACRE. es una sociedad con domicilio en C/Noviciado 18- 28015 MADRID. El correo electrónico de contacto es: info@acre.tienda
+Las presentes Condiciones de Uso regulan el acceso y la utilización del sitio web accesible a través de la url www.acre.tienda (el “Sitio Web”) titularidad de ACRE cuya finalidad es la promoción y comercialización de los productos ofrecidos en el Sitio Web (los “Productos”) para su posterior adquisición por parte de los usuarios del Sitio Web (los “Usuarios”).
+ El acceso al Sitio Web o su uso por el Usuario implica necesariamente, y sin reservas, el sometimiento y aceptación de las presentes Condiciones de Uso y Política de Privacidad. Por tanto, se recomienda que el Usuario las lea de detenidamente cada vez que quiera acceder al Sitio Web
 
-@app.get('/auctions')
-def list_auctions(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return crud.get_auctions(db, skip=skip, limit=limit)
 
-@app.post('/auctions')
-def create_auction(title: str, description: str, starting_price: float, db: Session = Depends(get_db)):
-    auction = models.Auction(title=title, description=description, starting_price=starting_price)
-    return crud.create_auction(db, auction)
+A.2. DESCRIPCIÓN DEL SERVICIO
+
+
+
+El Sitio Web está configurado como una plataforma destinada a promocionar y comercializar los Productos para su adquisición por parte de los Usuarios. El Usuario que desee adquirir los Productos deberá leer y aceptar las Condiciones Generales de Contratación del Sitio Web mediante la validación de la casilla habilitada a tal efecto en el propio Sitio Web.
+ACRE pondrá los medios razonables a disposición del Usuario para que los contenidos y Productos incluidos en el Sitio Web sean exactos y estén actualizados. No obstante, ACRE no está en condiciones de poder garantizar esta circunstancia en todo momento. Asimismo, ACRE no será responsable del funcionamiento de las redes de comunicación que posibilitan el funcionamiento del Sitio Web, ya sean fijas o móviles, cuya responsabilidad incumbe enteramente a prestadores ajenos a ACRE.
+Si lo considera oportuno, ACRE podrá suspender de forma inmediata la prestación del servicio y, en su caso, retirar los contenidos que estime incorrectos o ilegales, ya lo realice a su exclusivo criterio, ya a petición de tercero afectado o de autoridad competente, sin que dicha suspensión pueda dar lugar a indemnización alguna.
+
+
+A.3. UTILIZACIÓN DEL SITIO WEB
+
+
+
+ Los Usuarios serán enteramente responsables del correcto uso del Sitio Web con sujeción a la legalidad vigente, sea nacional o internacional, así como a los principios de buena fe, la moral, las buenas costumbres y el orden público, y con el compromiso de observar diligentemente cualquier instrucción adicional que pueda serle impartida por ACRE .
+Los Usuarios se comprometen a abstenerse de realizar determinados comportamientos contrarios a la legalidad, incluyendo, entre otros, los siguientes: alterar o modificar parte alguna del Sitio Web, eludiendo, desactivando o manipulando de cualquier forma las funciones del Sitio Web; introducir virus informáticos, archivos defectuosos, o cualquier otro programa informático que pueda provocar daños o alteraciones en los contenidos, programas o sistemas de ACRE.
+Nuestro sistema antivirus es razonablemente seguro. Por todo ello, ACRE no se hace responsable de los potenciales daños o errores que, debidos a la presencia de cualquier virus, pueda sufrir el sistema informático (hardware o software) de los Usuarios cuando éstos accedan al Sitio Web o lo utilicen.
+
+
+A.4. ENLACES A TERCEROS
+
+
+
+ACRE no se responsabiliza de aquellos contenidos, productos o servicios alojados en sitios web de terceros que sean accesibles, directa o indirectamente, por medio de enlaces desde el Sitio Web.ACRE
+
+
+A.5. PROPIEDAD INTELECTUAL E INDUSTRIAL
+
+
+
+Todos los derechos de propiedad intelectual sobre el Sitio Web pertenecen en exclusiva a ACRE . Se prohíbe expresamente toda forma de reproducción, distribución, comunicación pública, puesta a disposición, modificación, transformación y cualquier otro acto de explotación no consentido por ACRE de los contenidos que integran el Sitio Web, así como de las bases de datos y del programa informático que permiten el funcionamiento del mismo.
+Todas las marcas y logotipos que aparecen en el Sitio Web son propiedad de ACRE o de empresas proveedoras de ACRE , y se hallan protegidas por derechos de propiedad industrial. Queda terminantemente prohibido cualquier uso de las marcas, nombres comerciales, denominaciones sociales, rótulos de establecimiento, logotipos o cualesquiera otros signos distintivos que aparezcan en el Sitio Web.
+
+
+A.6. MODIFICACIÓN DE LAS CONDICIONES DE USO
+
+
+
+ACRE se reserva el derecho de modificar las presentes Condiciones de Uso, de acuerdo con la legislación aplicable en cada momento, de lo que informará debidamente en el Sitio Web, por lo que se recomienda a los Usuarios que revisen periódicamente las presentes Condiciones de Uso.
+
+
+B. POLÍTICA DE PRIVACIDAD
+
+
+
+La presente Política de Privacidad se aplica al Sitio Web titularidad de ACRE , cuya finalidad es la promoción y comercialización de los Productos.
+
+
+B.1. TRATAMIENTO DE LOS DATOS DE CARÁCTER PERSONAL
+
+
+
+De conformidad con la legislación vigente en materia de protección de datos de carácter personal y en particular, la Ley Orgánica 15/1999 de 13 de Diciembre de Protección de Datos de Carácter Personal y su Reglamento de Desarrollo 1720/2007, de 21 de diciembre, así como toda la normativa europea y española aplicable a esta materia, los datos que ACRE facilite el Usuario del Sitio Web, serán incorporados para su tratamiento a un fichero debidamente inscrito por ACRE en la Agencia Española de Protección de Datos con la finalidad de poder identificarle y contactarle, así como facilitarle cualquier información que solicite.
+En cualquier caso, el Usuario podrá ejercer sus derechos de acceso, rectificación, oposición y cancelación, los cuales podrán ser ejercidos por él y, en su caso, por quien lo represente, mediante solicitud escrita y firmada dirigida a ACRE ,C/Noviciado 18 28015 MADRID.
+Dicha solicitud deberá ir acompañada de los siguientes datos: nombre y apellidos del Usuario, domicilio a efectos de notificaciones y fotocopia del documento nacional de identidad o del pasaporte. En el caso de representación, deberá probarse ésta mediante documento fehaciente.
+ACRE podrá también recopilar la dirección IP del Usuario así como los datos de navegación dentro del Sitio Web con el fin de identificar posibles usos fraudulentos del Sitio Web.
+ACRE se compromete a mantener la privacidad de los datos facilitados por el Usuario. Asimismo, ACRE garantiza que ha adoptado los niveles de seguridad de protección de los datos personales legalmente requeridos, y ha instalado todos los medios y medidas técnicas a su alcance para evitar la pérdida, mal uso, alteración, acceso no autorizado y robo de los datos personales facilitados.
+El Usuario deberá notificar a ACRE cualquier modificación que se produzca en los datos que haya facilitado, respondiendo en cualquier caso de la veracidad y exactitud de los datos suministrados en cada momento.
+
+
+B.2. ENVIO DE COMUNICACIONES COMERCIALES
+
+
+
+El CLIENTE recibirá comunicaciones comerciales de ACRE tras la formalización de la relación contractual al amparo del articulo 21.2 de la LEY 34/2002, de 11 de julio, de servicios de la sociedad de la información y de comercio electrónico, suponiendo éste una excepción a la obligación de autorizar expresamente la recepción de comunicaciones comerciales cuando exista una relación contractual previa , siempre que el prestador hubiera obtenido de forma lícita los datos de contacto del destinatario y los empleara para el envío de comunicaciones comerciales referentes a productos o servicios de su propia empresa que sean similares a los que inicialmente fueron objeto de contratación con el cliente.
+El CLIENTE podrá renunciar en cualquier momento a recibir dichas comunicaciones enviando un correo electrónico a ACRE a la dirección de correo electrónico: info@acre.tienda , manifestando dicha intención de renuncia. Asimismo, esta posibilidad le será ofrecida al CLIENTE en cada comunicación comercial que reciba de ACRE.
+B.3. MENORES DE EDAD
+
+Los Productos sólo están disponibles para mayores de edad. Por lo tanto, aquellos que no cumplan con esta condición deberán abstenerse de suministrar información personal en el Sitio Web para ser incluida en las bases de datos de ACRE. Sin embargo, con el previo consentimiento de sus padres o tutores podrán proceder a la inclusión de sus datos personales en los formularios del Sitio Web.
+
+
+B.4. USO DE COOKIES
+
+
+
+ El Sitio Web utiliza cookies para ayudarle a personalizar su experiencia en línea. Las cookies son pequeños ficheros de datos que se alojan en el terminal del Usuario y que contienen cierta información de la visita al Sitio Web. El único propósito de las cookies es proporcionar funcionalidades para ahorrarle tiempo, sin que en ningún caso sea posible asociar cookies a los datos personales concretos de los Usuarios ni identificar a éstos a través de aquellas.
+El Usuario tiene la posibilidad de aceptar o rechazar las cookies. Aunque la mayoría de navegadores aceptan automáticamente cookies, el Usuario puede modificar la configuración del explorador para rechazarlas. Si decide rechazar las cookies, no podrá aprovechar en su totalidad las características interactivas de los servicios del Sitio Web.
+
+B.5. MODIFICACIONES A ESTA POLÍTICA DE PRIVACIDAD
+
+
+
+ACRE se reserva el derecho de modificar esta Política de Privacidad de acuerdo con la legislación aplicable en cada momento y recomienda al Usuario que revise periódicamente esta Política de Privacidad para estar informado de cómo ACRE protege su información.
+La Comisión Europea facilita una plataforma de resolución de litigios en línea que se encuentra disponible en el siguiente enlace: http://ec.europa.eu/consumers/odr/. Los consumidores podrán someter sus reclamaciones a través de la plataforma de resolución de litigios en línea”.
+
+
+
+LEY APLICABLE Y JURISDICCIÓN
+
+
+
+Las presentes Condiciones de Uso y Política de Privacidad quedan sujetas al ordenamiento jurídico español.
+Para la resolución de cualquier conflicto que pudiera derivarse del acceso al Sitio Web, el Usuario y ACRE acuerdan someterse expresamente a los juzgados y tribunales de la ciudad de Madrid, con renuncia a cualquier otro fuero general o especial que les pudiera corresponde
