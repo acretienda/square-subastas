7d4856e54e134cae7b2c008297eae0543652c5d0
acretienda <acre.tienda@gmail.com>
2025-09-21 00:32:54 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ⚡ Coge la URL de la base de datos desde la variable de entorno
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://subastas_db_user:fJc1OootrjKeyIHxaIsj6ps0DUnjhY9r@dpg-d33j79idbo4c73b97s1g-a:5432/subastas_db")
+# Copia aquí la URL interna que Render te da
+DATABASE_URL = "postgresql://user_subastas_db:ymB6UlzVr9AouYVN9BUbSOVwKAYfFLBv@dpg-d37ik73uibrs7393iu50-a/subastas_db_9nmw"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
-# 👉 Dependencia para usar en los endpoints
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
