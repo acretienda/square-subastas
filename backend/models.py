67bac209bedcd85b045698f5c9c3dd2d8bcc6e35
acretienda <acre.tienda@gmail.com>
2025-09-21 00:31:33 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,9 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String
 from database import Base
 
-class Product(Base):
-    __tablename__ = "products"
+class User(Base):
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    description = Column(String(500), nullable=True)
-    price = Column(Float, nullable=False)
-    stock = Column(Integer, default=0)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
