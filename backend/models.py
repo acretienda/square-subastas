9396c243d143d8fd8533a4c40fcab5c1d1581e60
acretienda <acre.tienda@gmail.com>
2025-09-22 23:47:09 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,20 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
 from config import Base
-from datetime import datetime
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+
+    auctions = relationship("Auction", back_populates="owner")
 
 class Auction(Base):
     __tablename__ = "auctions"
-
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String)
-    starting_price = Column(Float)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    titulo = Column(String, index=True)
+    precio_inicial = Column(Float)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="auctions")
