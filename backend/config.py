0ade09b01941ef622a741f74d2ff252598cf617d
acretienda <acre.tienda@gmail.com>
2025-09-23 00:49:21 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,2 +1,9 @@
-# URL interna de la base de datos en Render
-DATABASE_URL = "postgresql://user_subastas_db:ymB6UlzVr9AouYVN9BUbSOVwKAYfFLBv@dpg-d37ik73uibrs7393iu50-a/subastas_db_9nmw"
+import os
+
+class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://user_subastas_db:ymB6UlzVr9AouYVN9BUbSOVwKAYfFLBv@dpg-d37ik73uibrs7393iu50-a/subastas_db_9nmw")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecretkey123")
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+settings = Settings()
